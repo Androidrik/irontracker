a1ac3102895ef9f6d969d21005b27350bf8a4549
Androidrik <rikbottel@gmail.com>
2014-01-02 17:04:21 +0100

    Resources verplaatst naar nl.programmers.web.

--- src/main/java/nl/brogrammers/web/IrontrackerWebPage.java
+++ src/main/java/nl/brogrammers/web/IrontrackerWebPage.java
@@ -2,9 +2,13 @@ package nl.brogrammers.web;
 
 import org.apache.wicket.markup.head.CssReferenceHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.html.IHeaderContributor;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.request.resource.CssResourceReference;
+import org.wicketstuff.jslibraries.JSLib;
+import org.wicketstuff.jslibraries.Library;
+import org.wicketstuff.jslibraries.VersionDescriptor;
 
 public class IrontrackerWebPage extends WebPage
 {
@@ -17,13 +21,22 @@ public class IrontrackerWebPage extends WebPage
     @Override
     public void renderHead(IHeaderResponse response)
     {
+	IHeaderContributor jQuery = JSLib
+		.getHeaderContribution(VersionDescriptor
+			.alwaysLatest(Library.JQUERY));
+	jQuery.renderHead(response);
+
+	IHeaderContributor jQueryUI = JSLib
+		.getHeaderContribution(VersionDescriptor
+			.alwaysLatest(Library.JQUERY_UI));
+	jQueryUI.renderHead(response);
+
+	response.render(CssReferenceHeaderItem
+		.forReference(new CssResourceReference(
+			IrontrackerWebPage.class, "bootstrap/css/bootstrap.css")));
 	response.render(CssReferenceHeaderItem
 		.forReference(new CssResourceReference(
 			IrontrackerWebPage.class,
-			"assets/bootstrap/css/bootstrap-responsive.css")));
-	response.render(CssReferenceHeaderItem
-		.forReference(new CssResourceReference(
-			IrontrackerWebPage.class,
-			"assets/bootstrap/css/bootstrap.css")));
+			"bootstrap/css/bootstrap-responsive.css")));
     }
 }
